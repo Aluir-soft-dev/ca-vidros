7d14b929ed625f0814ee92be43c8519185fc65c5
Gabriel Rosa <90804775+Aluir-soft-dev@users.noreply.github.com>
2026-02-28 09:36:45 -0300

    Refactor ReviewsSection layout for full width

--- src/app/Components/ReviewsSection.tsx
+++ src/app/Components/ReviewsSection.tsx
@@ -169,12 +169,12 @@ export default function ReviewsSection() {
                 <div className="w-full flex justify-center lg:justify-start ">
                   <div
                     className="
-                      grid
-                      grid-cols-1
-                      sm:grid-cols-2
-                      lg:grid-cols-3
-                      gap-4
-                      justify-items-center
+                     grid
+    grid-cols-1
+    sm:grid-cols-2
+    lg:grid-cols-3
+    gap-4
+    w-full
                     "
                   >
                     {currentPageItems.map((r) => (
